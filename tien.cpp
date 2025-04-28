2ef3ebc456bd8975af204a9b1beeda74ca5ce990
huy tien <hohuytien05@gmail.com>
2025-04-28 20:18:12 +0700

    sub

---
+++ tien.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int a = 1, b = 2, c = 3;
+
+int add(int a, int b, int c);
+int subtract(int a, int b, int c);
+int multiply(int a, int b, int c);
+
+int main() {
+	
+	
+    return 0;
+} 
