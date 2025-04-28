3dfb7a3e1c84a21ebdfe601c1b13e6d737600ed4
hieupv-25 <hpham6383@gmail.com>
2025-04-28 19:55:04 +0700

    Add files via upload
    Mọi người clone về và làm theo yêu cầu

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int a = 1, b = 2, c = 3;
+
+// Prototypes cho các hàm từ thành viên
+int add(int a, int b, int c);
+int subtract(int a, int b, int c);
+int multiply(int a, int b, int c);
+
+int main() {
+    // Sẽ được cập nhật sau khi có code từ thành viên
+    return 0;
+}
